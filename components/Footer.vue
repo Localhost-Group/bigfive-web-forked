609d2c8256286849c86ad2f764906c444bd7acb0
elijahting <elijah.tingmk@gmail.com>
2023-07-11 21:16:17 +0800

    edit footer layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,17 +7,14 @@
         class="pl-0 pt-0"
       >
         <v-row class="footer-wrapper pl-0">
-          <v-col class="footer-heading">
-            <a href="https://wa.me/message/DTN4ST4TTRBWG1" target="_blank">
-              <button class="button"><span style="line-height: 1.2;">预约咨询</span><span style="line-height: 1.2;">Book a Consultation</span></button>
-            </a><br><br>
-            <h5><a href="https://glows.my" target="_blank" style="color: darkblue; font-size: 1.5rem;">WISDOER GLOBAL</a></h5>
-          </v-col>
-
           <v-col
             cols="12"
             class="footer-about"
           >
+          <a href="https://wa.me/message/DTN4ST4TTRBWG1" target="_blank">
+              <button class="button"><span style="line-height: 1.2;">预约咨询</span><span style="line-height: 1.2;">Book a Consultation</span></button>
+            </a><br>
+            <h5><a href="https://glows.my" target="_blank" style="color: darkblue; font-size: 1.5rem;">WISDOER GLOBAL</a></h5><br>
             No. 18, 1st Floor<br>
             Lorong Tunku Abdul Rahman 8A<br>
             96000 Sibu, Sarawak, Malaysia<br><br>
